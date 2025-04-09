4ec2eb0cbc648acf70c09dd671592e78b15934b6
Neil Yeung <neil.y.yeung@gmail.com>
2025-04-09 19:55:32 +0000

    cors issues and need to find a clean way to do admin dashboard

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@
 
 import { useUser } from '@auth0/nextjs-auth0'
 import Link from 'next/link'
-import Image from 'next/image'
+// import Image from 'next/image'
 
 export default function Header() {
   const { user, error, isLoading } = useUser()
@@ -14,25 +14,25 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center px-6 py-4 border-b shadow-sm lg:pl-24 bg-light-primary dark:bg-dark-primary">
 
-  <div className="text-xl font-bold">Vinci Stockalyzer</div>
-  <nav className="flex items-center gap-4">
-    {user ? (
-      <Link
-        href="auth/logout"
-        className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition"
-      >
-        Logout
-      </Link>
-    ) : (
-      <Link
-        href="auth/login"
-        className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
-      >
-        Login
-      </Link>
-    )}
-  </nav>
-</header>
+      <div className="text-xl font-bold">Vinci Stockalyzer</div>
+      <nav className="flex items-center gap-4">
+        {user ? (
+          <Link
+            href="/auth/logout"
+            className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition"
+          >
+            Logout
+          </Link>
+        ) : (
+          <Link
+            href="/auth/login"
+            className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+          >
+            Login
+          </Link>
+        )}
+      </nav>
+    </header>
 
   )
 }
