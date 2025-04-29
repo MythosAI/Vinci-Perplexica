5e010278a2077bfb37e32b6083757980c343df55
jrfibel <jason.feibelman@gmail.com>
2025-04-29 14:02:32 -0400

    rm local changes

--- rebuild.sh
+++ rebuild.sh
@@ -3,11 +3,10 @@
 start_time=$(date +%s)
 
 echo "Bringing down Docker Compose services and removing volumes..."
-# docker compose down -v
-docker stop perplexica-stockalyzer-app-1
+docker compose down -v
 
-# echo "Pruning unused Docker data..."
-# docker system prune -af
+echo "Pruning unused Docker data..."
+docker system prune -af
 
 echo "Rebuilding services without cache..."
 docker compose build --no-cache
